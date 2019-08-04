44a9a911b993aa434e0e360dab2bd95055dafdab
AlNadraga <nadraga.alexandr@yandex.ru>
2019-08-04 15:54:04 +0300

    added comments and some small fixes

--- src/test/kotlin/steps/UserListSteps.kt
+++ src/test/kotlin/steps/UserListSteps.kt
@@ -3,6 +3,7 @@ package steps
 import io.restassured.RestAssured
 import io.restassured.path.json.JsonPath.from
 import io.restassured.response.Response
+import net.serenitybdd.core.Serenity
 import net.thucydides.core.annotations.Step
 
 import org.hamcrest.Matchers.`is`
@@ -16,27 +17,33 @@ const val TOTAL_USERS = 12
 open class ListUsersResponse {
 
     private var response: Response? = null
+    private var users_counter:Int = 0
 
+    //Получаем ответ тут и сохраняем его
     @Step
     open fun getUserListByNumber(page_num: Int) {
         response = RestAssured.`when`().get(LIST_USERS_URL + "$page_num")
     }
-
+    //Проверяем, что ответ пришел и все хорошо
     @Step
     open fun isExecutedSuccessful() {
         response!!.then().statusCode(200)
     }
-
+    //Получаем юзера из ответа по id и проверяем на соответсвие
     @Step
-    open fun verifyUserById(id: Int, first_name: String, last_name: String) {
+    open fun checkUserById(id: Int, first_name: String, last_name: String) {
         response!!.then().body("data.find {it.id == $id}.first_name", `is`(first_name))
             .body("data.find {it.id == $id}.last_name", `is`(last_name))
     }
-
+    //Тут проверяем, что все поля присутсвуют
     @Step
-    open fun verifyJSONFields() {
+    open fun checkDataField() {
         val users_per_page = from(response!!.asString()).getInt("per_page")
+        //Если вернулось поле data нулевой длины то далее нет смысла проверять
         response!!.then().body("data.size()", not(0))
+        //Проверяем каждый элемент в data и смотрим, что везде есть нужные поля. Касательно полей было сказано, что
+        //все поля обязательные. Немного не понял, обязательно, всмысле, их наличие, или то, что они не пустые, поэтому
+        //проверяю и на наличие полей и на то, что они не пустые
         for (i in 0 until users_per_page) {
             response!!.then().body("data[$i]", hasKey("id")).and().body("data[$i].id", not(nullValue()))
                 .body("data[$i]", hasKey("email")).and().body("data[$i].email", not(nullValue()))
@@ -44,21 +51,20 @@ open class ListUsersResponse {
                 .body("data[$i]", hasKey("last_name")).and().body("data[$i].last_name", not(nullValue()))
         }
     }
-
+    //Проверка на количество пользователей.Проверяю, что поле total равно 12, а также считаю ползователей, чтобы потом
+    //сравнить уже количество пользователей из data с 12
     @Step
-    open fun checkTotalUser() {
+    open fun checkTotalUserAtPage() {
+        //Сравниваю с константой
         response!!.then().body("total", `is`(TOTAL_USERS))
-        val total_users = from(response!!.asString()).getInt("total")
-        val total_pages = from(response!!.asString()).getInt("total_pages")
-        var users_counter = 0
-
-        for (i in 2..total_pages + 1) {
-            response!!.then().body("data.size()", not(0))
-                .body("total", `is`(TOTAL_USERS))
-                .body("data.size()", `is`(from(response!!.asString()).getInt("per_page")))
-            users_counter += from(response!!.asString()).getInt("data.size()")
-            getUserListByNumber(i)
-        }
-        assert(users_counter == total_users, { "Users counter ($users_counter) != total users ($total_users)" })
+            //Также смотрим что поле с количеством пользователей на странице соответствует количеству в data
+            .body("data.size()", `is`(from(response!!.asString()).getInt("per_page")))
+        users_counter += from(response!!.asString()).getInt("data.size()")
+    }
+    //Тут уже проверяю, что именно посчитанных пользователей по страницам совпадает с полем total
+    @Step
+    open fun checkTotalUsers(){
+        response!!.then().body("total",`is`(users_counter))
+        users_counter = 0
     }
 }
--- src/test/kotlin/tests/ReqresTests.kt
+++ src/test/kotlin/tests/ReqresTests.kt
@@ -9,26 +9,38 @@ import org.junit.runner.RunWith
 
 @RunWith(SerenityRunner::class)
 class UserListTest {
+    //Все тесты разбиты на шаги, которые объединяем в один тест. Код шагов - UserListSteps.kt
     @Steps
     lateinit var usersValid: ListUsersResponse
+    //Валидация значений полей JSON структуры респонза, например проверяем, что юзера с id=1
+    //зовут “George”.
     @Test
     fun verifyThatUserWithId1HasNameGeorge(){
+        //Шаг получения ответа на запрос
         usersValid.getUserListByNumber(1)
+        //Проверяем, что успешно получили ответ
         usersValid.isExecutedSuccessful()
-        usersValid.verifyUserById(1,"George", "Bluth")
+        //Проверяем, что пользовтеля с id 1 зовут George Bluth
+        usersValid.checkUserById(1,"George", "Bluth")
     }
-
+    //валидация структуры объекта data в респонзе (т.е. что все обязательные поля всегда
+    //присутствуют. Мы полагаем, что все поля mandatory)
     @Test
     fun verifyDataFields(){
         usersValid.getUserListByNumber(1)
         usersValid.isExecutedSuccessful()
-        usersValid.verifyJSONFields()
+        usersValid.checkDataField()
     }
-
+    //проверка, что сервис всегда отдает фиксированное кол-во юзеров суммарно по всем страницам. Проверяем, что
+    //на каждой странице поле total равно 12, а также считаем пользователей сами и сравниваем уже посчитанных
     @Test
     fun verifyTotalUser(){
-        usersValid.getUserListByNumber(1)
-        usersValid.isExecutedSuccessful()
-        usersValid.checkTotalUser()
+        for(i in 1..4) {
+            usersValid.getUserListByNumber(i)
+            usersValid.isExecutedSuccessful()
+            usersValid.checkDataField()
+            usersValid.checkTotalUserAtPage()
+        }
+        usersValid.checkTotalUsers()
     }
 }
